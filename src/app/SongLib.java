dc1e53b9c69883e25901314ce3ea0d0a1f58d1af
Zabir Rahman <zabirrahman@gmail.com>
2020-09-30 02:49:22 -0400

    Added Song Detail pane

--- src/app/SongLib.java
+++ src/app/SongLib.java
@@ -2,15 +2,22 @@ package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import view.Controller;
 
 public class SongLib extends Application {
-
-    @Override
+	
+	@Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/mainwindow.fxml"));
+    	FXMLLoader rootLoader = new FXMLLoader();
+    	rootLoader.setLocation(getClass().getResource("/view/mainwindow.fxml"));
+    	BorderPane root = (BorderPane)rootLoader.load();
+    	
+    	Controller rootController = rootLoader.getController();
+        rootController.init();
+        
         primaryStage.setTitle("Song Library");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
